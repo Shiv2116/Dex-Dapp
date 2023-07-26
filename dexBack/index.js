c4244b0a3dff50d00c34c5f252fa498b25a33dc8
Shiv2116 <114937365+Shiv2116@users.noreply.github.com>
2023-07-26 10:32:43 +0530

    first commit

--- dexBack/index.js
+++ dexBack/index.js
@@ -5,10 +5,26 @@ const cors = require("cors");
 require("dotenv").config();
 const port = 3001;
 
-app.use(cors());
+app.use(cors());//cross origin resource sharing enabled
 app.use(express.json());
 
 app.get("/tokenPrice", async (req, res) => {
+
+  const {query} = req;
+  const responseOne = await Moralis.EvmApi.token.getTokenPrice({
+    address: query.addressOne
+  })
+  const responseTwo = await Moralis.EvmApi.token.getTokenPrice({
+    address: query.addressTwo
+  })
+
+  const usdPrices = {
+    tokenOne:responseOne.raw.usdPrice,
+    tokenTwo:responseTwo.raw.usdPrice,
+    ratio:responseOne.raw.usdPrice/responseTwo.raw.usdPrice
+   }
+
+
   return res.status(200).json(usdPrices);
 });
 
